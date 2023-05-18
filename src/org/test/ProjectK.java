98cb45bde9f95429c7d36d385d67b5645bfcee40
Kalyani1444 <rusthum768@gmail.com>
2023-05-18 20:49:12 +0530

    teck

--- src/org/test/ProjectK.java
+++ src/org/test/ProjectK.java
@@ -1,6 +1,8 @@
 package org.test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class ProjectK {
@@ -14,6 +16,10 @@ public class ProjectK {
 		
 		driver.get("https://www.facebook.com/");
 
+WebElement email = driver.findElement(By.id("email"));
+email.click();
 
+WebElement email1 = driver.findElement(By.id("pass"));
+email1.click();
 	}
 }
