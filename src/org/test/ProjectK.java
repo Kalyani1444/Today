a7483b4084e83ed3a023b15a61b7a9905d4bab83
Kalyani1444 <rusthum768@gmail.com>
2023-05-18 20:12:06 +0530

    master

---
+++ src/org/test/ProjectK.java
@@ -0,0 +1,19 @@
+package org.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class ProjectK {
+
+	public static void main(String[] args) {
+		
+	
+		WebDriver driver;
+		
+		driver = new EdgeDriver();
+		
+		driver.get("https://www.facebook.com/");
+
+
+	}
+}
